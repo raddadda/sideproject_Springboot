652f42df0f6532672beb054a977df0d3db5346ce
raddadda <cyc123m@gmail.com>
2024-09-10 19:07:48 +0900

    first commit

---
+++ src/main/java/com/example/SpringbootJavaProject/SpringbootJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootJavaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJavaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringbootJavaProject/SpringbootJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootJavaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
